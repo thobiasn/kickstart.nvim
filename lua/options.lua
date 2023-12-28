7706ab5078baeafbf2fc03ea117c661b039b14da
ThobiasN <thobias.nordgaard@gmail.com>
2023-12-28 09:48:25 +0100

    add catppuccin

--- lua/options.lua
+++ lua/options.lua
@@ -39,4 +39,7 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- set colorscheme
+vim.cmd.colorscheme "catppuccin"
+
 -- vim: ts=2 sts=2 sw=2 et
