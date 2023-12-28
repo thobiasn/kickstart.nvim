78f4e14378135d9c1b0d59565dcb8dbeebdbab35
ThobiasN <thobias.nordgaard@gmail.com>
2023-12-28 08:50:39 +0100

    add netrw bind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,4 +25,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- map netrw
+vim.keymap.set('n', '<leader>f', vim.cmd.Ex, { desc = '[F]ile explorer' })
+
 -- vim: ts=2 sts=2 sw=2 et
