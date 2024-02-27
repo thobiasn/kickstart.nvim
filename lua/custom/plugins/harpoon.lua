99cfe28aecad293df1e4ac82d145cba07b9cf06a
ThobiasN <thobias.nordgaard@gmail.com>
2024-02-27 11:14:13 +0100

    add telescope support for harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,8 +6,26 @@ return {
     local harpoon = require('harpoon')
     harpoon:setup()
 
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
     vim.keymap.set("n", "<C-a>", function() harpoon:list():append() end, { desc = 'Harpoon: Add file'})
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon: Toggle menu'})
+    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = 'Harpoon: Toggle menu'})
 
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = 'Harpoon: Go to file 1'})
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = 'Harpoon: Go to file 2'})
@@ -17,5 +35,5 @@ return {
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = 'Harpoon: Go to next file'})
     vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = 'Harpoon: Go to previous file'})
-  end,
+  end
 }
